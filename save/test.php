556b582c51e7b42d855ed0f210647094da3f1479
LocatedMaple38 <locatedmapleh@gmail.com.com>
2025-02-22 10:22:10 -0700

    update

--- save/test.php
+++ save/test.php
@@ -1,23 +1,22 @@
 <!-- filepath: /c:/Users/Student/Desktop/code/github/theislidefamly.com/test.php -->
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = htmlspecialchars($_POST['email']);
-    $display_name = htmlspecialchars($_POST['display_name']);
-    $password = htmlspecialchars($_POST['password']);
-    $first_name = htmlspecialchars($_POST['first_name']);
-    $last_name = htmlspecialchars($_POST['last_name']);
-    $phone = htmlspecialchars($_POST['phone']);
-    $address = htmlspecialchars($_POST['address']);
-    $zip = htmlspecialchars($_POST['zip']);
-    $country = htmlspecialchars($_POST['country']);
-    $state = htmlspecialchars($_POST['state']);
+    $email = htmlspecialchars(string: $_POST['email']);
+    $display_name = htmlspecialchars(string: $_POST['display_name']);
+    $password = htmlspecialchars(string: $_POST['password']);
+    $first_name = htmlspecialchars(string: $_POST['fname']);
+    $last_name = htmlspecialchars(string: $_POST['lname']);
+    $phone = htmlspecialchars(string: $_POST['Phone']);
+    $address = htmlspecialchars(string: $_POST['address']);
+    $zip = htmlspecialchars(string: $_POST['zip']);
+    $country = htmlspecialchars(string: $_POST['country']);
+    $state = htmlspecialchars(string: $_POST['state']);
     
     $data = "Email: $email, Display Name: $display_name, Password: $password, First Name: $first_name, Last Name: $last_name, Phone: $phone, Address: $address, Zip: $zip, Country: $country, State: $state\n";
-    
-    $file = fopen("submissions.txt", "a+");
+
+    $file = fopen(filename: "submissions.txt", mode: "a+");
     fwrite(stream: $file, data: $data);
     fclose(stream: $file);
-    
-    echo "Thank you for signing up!";
+    header(header: "Location: https://locatedmaple38.github.io/theislidefamly.com/"); 
 }
 ?>
